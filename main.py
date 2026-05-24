b268d1de0a56d9ceb4820ebdf5d0c6f2521e649d
ashenafi <ashenafilaptop@gmail.com>
2026-05-24 15:36:47 +0300

    cll

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from database import Base, engine
-from routers import auth,products,users
+from fastapi.middleware.cors import CORSMiddleware
 
+from database import Base, engine
+from routers import auth, products, users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# CORS
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
 app.include_router(auth.router)
-app.include_router(products.router)                 
-app.include_router(users.router)                 
+app.include_router(products.router)
+app.include_router(users.router)
 
-
-
-# Serve uploaded files
+# Static files
 app.mount(
     "/uploads",
     StaticFiles(directory="uploads"),
